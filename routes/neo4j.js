162fa9c1762ba63470e36a5d8479eef70051805b
Damian Harasymczuk <harasymczuk@contecht.eu>
2018-05-08 15:11:04 +0200

    neo4j routes change

--- routes/neo4j.js
+++ routes/neo4j.js
@@ -5,23 +5,23 @@ var elementColors = {
   "Track": "#ff6666",
   "Switch": "#800000",
   "Connection": "#3d003d",
-}
+  "Signal": "#aafeaa",
+  "SpeedChange": "#ead7c7",
+  "GradientChange": "#2e2125",
+  "RadiusChange": "#695250",
+  "BufferStop": "#b8bcc8",
+  "PlatformEdge": "#db2323"
 
-var linkColors = {
-  "BEGINS": "#DAF7A6",
-  "ENDS": "#FF5733",
-  "HAS_SWITCH": "#C70039",
-  "HAS_TRACK_ELEMENT": "#3498DB",
-  "HAS_OCS_ELEMENT": "#45B39D",
 }
 
 
 module.exports = function(app) {
-    /*
-     * Get all tracks from the database (all nodes with the 'Track' label)
-     * Executes a cypher query.
-     *
-     * @response - an array of objects (graph nodes)
+   /**
+     * @api {GET} /api/v1/tracks
+     * @apiDescription Get all tracks from the database (all nodes with the 'Track' label). Executes a cypher query.
+     * @apiName GetTracks
+     * @apiGroup Neo4j
+     * @apiSuccess (200) {array} tracks an array of objects (graph nodes), representing the railway network
      */
     app.get('/api/v1/tracks', function(req, res) {
       db.cypher({
@@ -32,14 +32,13 @@ module.exports = function(app) {
       });
     })
 
-    /*
-     * Get all nodes related to the given track. Relation between the track and elements are RailMl relations.
-     * Executes a cypher query.
-     *
-     * Body parameters:
-     *     {string} id - a track id
-     *
-     * @response - an array of objects (graph nodes)
+   /**
+     * @api {POST} /api/v1/elements
+     * @apiDescription Get all nodes related to the given track. Relation between the track and elements are RailMl relations. Executes a cypher query.
+     * @apiName GetTrackElements
+     * @apiGroup Neo4j
+     * @apiParam {string} id  a track id
+     * @apiSuccess (200) {array} elements an array of objects (graph nodes), representing the given track and related elements
      */
     app.post('/api/v1/elements', function(req, res) {
       db.cypher({
@@ -53,17 +52,16 @@ module.exports = function(app) {
       });
     })
 
-    /*
-     * Creates a new node related to the given track.
-     * Executes a cypher query.
-     *
-     * Body parameters:
-     *     {string} label - a label for the new node
-     *     {string} relationship - relationship between a new node and the given track
-     *     {string} track_id - given track id
-     *     {string} properties - new node properties
-     *
-     * @response - an array of objects (graph nodes)
+   /**
+     * @api {POST} /api/v1/add/element
+     * @apiDescription Creates a new node related to the given track. Executes a cypher query.
+     * @apiName AddElement
+     * @apiGroup Neo4j
+     * @apiParam {string} label a label for a new node
+     * @apiParam {string} relationship relationship between a new node and the given track
+     * @apiParam{string} track_id given track id
+     * @apiParam {string} properties new node properties
+     * @apiSuccess (200) {boolean} created returns true
      */
     app.post('/api/v1/add/element', function(req, res) {
       db.cypher({
@@ -74,47 +72,53 @@ module.exports = function(app) {
         }
       }, function (err, results) {
         if (err) throw err;
-        res.json(results)
+        res.json(true)
       });
     })
 
-    /*
-     * Removes given element from the database.
-     * Executes a cypher query.
-     *
-     * Body parameters:
-     *     {string} label - a label for the removed node
-     *     {string} id - an id for the removed node
-     *
-     * @response - an array of objects (graph nodes)
+   /**
+     * @api {POST} /api/v1/remove/element
+     * @apiDescription Removes given element from the database. Executes a cypher query.
+     * @apiName RemoveElement
+     * @apiGroup Neo4j
+     * @apiParam {string} label  a label for the removed node
+     * @apiParam {string} id an id for the removed node
+     * @apiSuccess (200) {boolean} removed returns true
      */
     app.post('/api/v1/remove/element', function(req, res) {
       db.cypher({
         query: 'MATCH (n:`' + req.body.label + '` {id:' + req.body.id + '}) DETACH DELETE n', // string concatenations are not recommended
       }, function (err, results) {
         if (err) throw err;
-        res.json(results)
+        res.json(true)
       });
     })
 
-    /*
-     * Removes given track from the database.
-     * Executes a cypher query.
-     *
-     * Body parameters:
-     *     {string} id - an id for the removed node
-     *
-     * @response - an array of objects (graph nodes)
+   /**
+     * @api {POST} /api/v1/remove/track
+     * @apiDescription Removes given track from the database. Executes a cypher query.
+     * @apiName RemoveTrack
+     * @apiGroup Neo4j
+     * @apiParam {string} id an id for the removed node
+     * @apiSuccess (200) {boolean} removed returns true
      */
     app.post('/api/v1/remove/track', function(req, res) {
       db.cypher({
         query: 'MATCH (e)-[]-(t:`Track` {id:' + req.body.id + '})-[]-(s)-[:HAS_CONNECTION]-(c:Connection) DETACH DELETE t,e,s,c', // string concatenations are not recommended
       }, function (err, results) {
         if (err) throw err;
-        res.json(results)
+        res.json(true)
       });
     })
 
+    /**
+      * @api {POST} /api/v1/tracktograph
+      * @apiDescription Returns the given track and all related elements as a graph vis input. Executes a cypher query.
+      * @apiName TrackToGraph
+      * @apiGroup Neo4j
+      * @apiParam {string} id a track id
+      * @apiSuccess (200) {object} graph returns an object with nodes and edges, that can be loaded in vis.js
+      */
     app.post('/api/v1/tracktograph', function(req, res) {
       db.cypher({
         query: 'MATCH (n:Track {id: "' + req.body.id + '"})-[r]->(e) RETURN n,r,e', // string concatenations are not recommended
@@ -154,6 +158,15 @@ module.exports = function(app) {
       });
     })
 
+    /**
+      * @api {POST} /api/v1/elementstograph
+      * @apiDescription Node expanding. Returns all related nodes to the given one. Executes a cypher query.
+      * @apiName ElementsToGraph
+      * @apiGroup Neo4j
+      * @apiParam {string} label a given node label
+      * @apiParam {string} id a given node id
+      * @apiSuccess (200) {object} graph returns an object with nodes and edges, that can be loaded in vis.js
+      */
     app.post('/api/v1/elementstograph', function(req, res) {
       db.cypher({
         query: 'MATCH (n:`' + req.body.label + '` {id: "' + req.body.id + '"})-[r]-(e) WHERE TYPE(r)<>"HAS_TRACK" RETURN n,r,e', // string concatenations are not recommended
@@ -182,12 +195,37 @@ module.exports = function(app) {
       });
     })
 
+    /**
+      * @api {POST} /api/v1/neighbourhood
+      * @apiDescription Returns all not related to the railway network objects within the given distance to the track. Executes a cypher query.
+      * @apiName Neighbourhood
+      * @apiGroup Neo4j
+      * @apiParam {string} wkt a valid wkt string (multipolygon or polygon)
+      * @apiSuccess (200) {object} results returns an array of spatial objects (graph nodes)
+      */
     app.post('/api/v1/neighbourhood', function(req, res) {
       db.cypher({
         query: "CALL spatial.intersects('buildings', '" + req.body.wkt + "')"
       }, function (err, results) {
         if (err) throw err;
         res.json(results)
+      });
+    })
+
+    /**
+      * @api {GET} /api/v1/spatial/layers
+      * @apiDescription Returns all spatial layers existing in the database. Executes a cypher query.
+      * @apiName Neighbourhood
+      * @apiGroup Neo4j
+      * @apiSuccess (200) {array} result returns an array of strings
+      */
+    app.get('/api/v1/spatial/layers', function(req,res){
+      db.cypher({
+        query: "CALL spatial.layers()"
+      }, function (err, layers) {
+        if (err) throw err;
+        var result = layers.map(function(layer){return layer.name})
+        res.json(result)
       });
     })
 
